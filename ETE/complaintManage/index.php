cc965a607256bfa6ccb22e36e3a1dcdd4aeb5134
MrDargad <mah.dargad@gmail.com>
2024-04-23 16:45:53 +0530

    ete folder created

---
+++ ETE/complaintManage/index.php
@@ -0,0 +1,18 @@
+<!--  Develop a app using PHP and MySQl for Complaint Management System.
+ -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Complaint Registration form</title>
+</head>
+<body>
+    <h1>Customer Complaint Registration Form</h1>
+    <form action="Complaint.php" method="post">
+        
+
+    </form>
+</body>
+</html>
